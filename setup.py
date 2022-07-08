136e3512aba7b75a74d377c8f5b67c331548c9e1
Neil Graham <grahamneiln@gmail.com>
2022-07-08 18:41:27 -0400

    Updated version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path, getcwd
 
 setup(
     name="rdfhash",
-    version="0.2.6",
+    version="0.2.7",
     author="Neil Graham",
     author_email="grahamneiln@gmail.com",
     url="https://github.com/NeilGraham/rdfhash",
